4af51742e31e1c1a54793c4257e2505679b03f84
Helmut Grohne <h.grohne@cygnusnetworks.de>
2013-03-05 14:50:05 +0100

    distinguish block devices from disks
    Fact names change. Block devices are listed in "block_devices" and disks
    associated with a particular block device are listed in
    "block_disks_#{dev}". A main advantage here is that we can list drivers
    of block devices without knowing anything about the backing disks.

--- disks.rb
+++ disks.rb
@@ -13,15 +13,41 @@ def get_driver(device)
 	return driverpath.split(/\//).last
 end
 
+class BlockInfo
+	attr_accessor :device
+	attr_accessor :disks
+	def initialize(device)
+		@device = device
+		@disks = []
+	end
+	def driver
+		@driver ||= get_driver(@device)
+	end
+	def vendor
+		@vendor ||= File.read("/sys/block/#{device}/device/vendor").rstrip
+	end
+end
+
 class DiskInfo
 	attr_accessor :devid
-	attr_accessor :device
+	def initialize(devid)
+		@devid = devid
+	end
+end
+
+class SmartDiskInfo < DiskInfo
+	attr_accessor :smartdev
+	attr_accessor :smarttype
 	attr_accessor :model
 	attr_accessor :serial
-	def initialize(devid, device, type, smartdev)
-		@devid = devid
-		@device = device
-		output = Facter::Util::Resolution.exec("smartctl -i -d #{type} /dev/#{smartdev}")
+	def initialize(devid, smartdev, smarttype)
+		super(devid)
+		@smartdev = smartdev
+		@smarttype = smarttype
+		run_smartctl
+	end
+	def run_smartctl
+		output = Facter::Util::Resolution.exec("smartctl -i -d #{@smarttype} /dev/#{@smartdev}")
 		raise "missing smartctl?" unless output
 		@model = output[/^Device Model: +(.*)/,1]
 		@model = output[/^Product: +(.*)/,1] unless @model
@@ -29,54 +55,58 @@ class DiskInfo
 		raise "model not found for #{devid}" unless @model
 		raise "serial not found for #{devid}" unless @serial
 	end
-
-	def driver
-		return get_driver(@device)
+	def model
+		run_smartctl unless @model
+		return @model
 	end
-
-	def vendor
-		return File.read("/sys/block/#{device}/device/vendor").rstrip
+	def serial
+		run_smartctl unless @serial
+		return @serial
 	end
 end
 
 if Facter.value(:kernel) == "Linux"
-	disks = []
-	Dir.glob("/sys/block/sd?/device/vendor") do |path|
+	blockdevs = []
+	Dir.glob("/sys/block/sd?") do |path|
 		begin
-			device = path[/sd./]
-			driver = get_driver(device)
+			device = BlockInfo.new(path[/sd./])
+			blockdevs << device
 
-			case driver
+			case device.driver
 			when "ahci"
-				disks << DiskInfo.new(device, device, "ata", device)
+				device.disks << SmartDiskInfo.new(device.device, device.device, "ata")
 			#when vendor == IBM-ESXS and driver == mptspi
-			#	disks << DiskInfo.new(device, device, "scsi", device)
+			#	device.disks << SmartDiskInfo(device.device, device.device, "scsi")
 			when "3w-9xxx"
-				raise "unknown backing device #{device} for 3w-9xxx" unless device == "sda"
+				raise "unknown backing device #{device} for 3w-9xxx" unless device.device == "sda"
 				(0..127).each do |n|
-					disks << DiskInfo.new("#{device}_#{n}", device, "3ware,#{n}", "twa0")
+					device.disks << SmartDiskInfo.new("#{device.device}_#{n}", "twa0", "3ware,#{n}")
 				end
 			when "megaraid_sas"
 				(0..32).each do |n|
 					begin
-						disks << DiskInfo.new("#{device}_#{n}", device, "megaraid,#{n}", device)
+						device.disks << SmartDiskInfo.new("#{device.device}_#{n}", device.device, "megaraid,#{n}")
 					rescue
 					end
 				end
+				raise "no disks found for #{device.device}" unless device.disks
 			# TODO: when vendor == LSI and driver == 3w-sas, devices apparently only expose serials via tw-cli
 			# TODO: when vendor == LSILOGIC and driver == mptspi run smartctl on the sgN backing devices
 			else
-				Facter.debug "unknown driver #{driver} for #{device}"
+				Facter.debug "unknown driver #{device.driver} for #{device.device}"
 			end
 		rescue
-			Facter.debug "exception while processing #{device}: " + $!.to_s
+			Facter.debug "exception while processing #{device.device}: " + $!.to_s
 		end
 	end
-	Facter.add(:disks) { setcode { (disks.collect(&:devid)).join(",") } }
-	disks.each do |disk|
-		Facter.add("disk_vendor_#{disk.devid}") { setcode { disk.vendor } }
-		Facter.add("disk_model_#{disk.devid}") { setcode { disk.model } }
-	Facter.add("disk_serial_#{disk.devid}") { setcode { disk.serial } }
-	Facter.add("disk_driver_#{disk.devid}") { setcode { disk.driver } }
+	Facter.add(:block_devices) { setcode { (blockdevs.collect(&:device)).join(",") } }
+	blockdevs.each do |device|
+		Facter.add("block_vendor_#{device.device}") { setcode { device.vendor } }
+		Facter.add("block_driver_#{device.device}") { setcode { device.driver } }
+		Facter.add("block_disks_#{device.device}") { setcode { (device.disks.collect(&:devid)).join(",") } }
+		device.disks.each do |disk|
+			Facter.add("disk_model_#{disk.devid}") { setcode { disk.model } }
+			Facter.add("disk_serial_#{disk.devid}") { setcode { disk.serial } }
+		end
 	end
 end
