b2c12b9c4778bc5552aeb920a0c7a2f5c972a243
Helmut Grohne <h.grohne@cygnusnetworks.de>
2013-03-12 12:37:58 +0100

    driver ata_piix also is for simple disks

--- disks.rb
+++ disks.rb
@@ -142,7 +142,7 @@ if Facter.value(:kernel) == "Linux"
 			blockdevs << device
 
 			case device.driver
-			when "ahci", "sata_via"
+			when "ahci", "ata_piix", "sata_via"
 				device.disks << SmartDiskInfo.new(device.device, device.device, "ata")
 			when "3w-9xxx"
 				raise "unknown backing device #{device.device} for 3w-9xxx" unless device.device == "sda"
